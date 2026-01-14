52fc7c56b137d8ab159cd1b08a1b3cdc3022eccc
ELSAPHABA <sb07907@naver.com>
2026-01-14 12:36:24 +0900

    예외 처리 모듈 분리

--- main.py
+++ main.py
@@ -3,41 +3,15 @@ from fastapi.responses import JSONResponse
 from fastapi.exceptions import RequestValidationError
 from starlette.exceptions import HTTPException as StarletteHTTPException
 from routers import post_router
+from exceptions import register_exception_handlers
 
 app = FastAPI()
 
 # 라우터 등록
 app.include_router(post_router.router)
 
-# 예외 처리기
-# 1. 필수 파라미터 누락 등 유효성 검사 실패 (400)
-@app.exception_handler(RequestValidationError)
-async def validation_exception_handler(request: Request, exc: RequestValidationError):
-    return JSONResponse(
-        status_code=status.HTTP_400_BAD_REQUEST,
-        content={"code": "invalid_request", "data": None}
-    )
-
-# 2. 허용되지 않은 메소드 요청 (405)
-@app.exception_handler(StarletteHTTPException)
-async def http_exception_handler(request: Request, exc: StarletteHTTPException):
-    if exc.status_code == 405:
-        return JSONResponse(
-            status_code=status.HTTP_405_METHOD_NOT_ALLOWED,
-            content={"code": "METHOD_NOT_ALLOWED", "data": None}
-        )
-    return JSONResponse(
-        status_code=exc.status_code,
-        content={"code": exc.detail, "data": None}
-    )
-
-# 3. 서버 내부 에러 (500)
-@app.exception_handler(Exception)
-async def global_exception_handler(request: Request, exc: Exception):
-    return JSONResponse(
-        status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
-        content={"code": "internal_server_error", "data": None}
-    )
+# 예외 처리기 등록
+register_exception_handlers(app)
 
 @app.get("/")
 async def root():
