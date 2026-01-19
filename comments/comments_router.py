ebcbc7172fe91a506f8ba0e86d57ca5aa528f8bf
ELSAPHABA <sb07907@naver.com>
2026-01-19 14:37:55 +0900

    Package by Feature Refactoring: 1. 'comments' 폴더로 댓글 기능 분리 2. 'posts' 폴더로 게시글 기능 분리 3. 의존성 수정

---
+++ comments/comments_router.py
@@ -0,0 +1,25 @@
+from fastapi import APIRouter, Depends
+from models import CommentCreate, CommentUpdate
+from comments import comments_controller
+from auth.auth_dependencies import get_current_user
+
+router = APIRouter(
+    prefix="/v1/posts",
+    tags=["comments"]
+)
+
+@router.get("/{postId}/comments")
+async def get_comments(postId: int, user: dict = Depends(get_current_user)):
+    return comments_controller.get_comments(postId)
+
+@router.post("/{postId}/comments", status_code=201)
+async def create_comment(postId: int, comment_data: CommentCreate, user: dict = Depends(get_current_user)):
+    return comments_controller.create_comment(postId, comment_data, user)
+
+@router.patch("/{postId}/comments/{commentId}")
+async def update_comment(postId: int, commentId: int, comment_data: CommentUpdate, user: dict = Depends(get_current_user)):
+    return comments_controller.update_comment(postId, commentId, comment_data, user)
+
+@router.delete("/{postId}/comments/{commentId}")
+async def delete_comment(postId: int, commentId: int, user: dict = Depends(get_current_user)):
+    return comments_controller.delete_comment(postId, commentId, user)
---
+++ posts/posts_router.py
@@ -0,0 +1,41 @@
+from fastapi import APIRouter, Query, Request, Depends
+from models import PostCreate, PostUpdate
+from posts import posts_controller
+from exceptions import validation_exception_handler
+from auth.auth_dependencies import get_current_user
+
+router = APIRouter(
+    prefix="/v1/posts",
+    tags=["posts"]
+)
+
+@router.get("")
+async def get_posts(request: Request, page: int = Query(1), size: int = Query(10)):
+    if page <= 0 or size <= 0:
+        return await validation_exception_handler(request, None)
+
+    # 컨트롤러 함수 호출
+    result = posts_controller.get_all_posts(page, size)
+    return result
+
+@router.get("/{postId}")
+async def get_post_detail(request: Request, postId: int):
+    # 컨트롤러에서 데이터 가져오기
+    post = await posts_controller.get_post_detail(request, postId)
+    
+    return {
+        "code": "post_retrieved",
+        "data": post
+    }
+
+@router.post("", status_code=201)
+async def create_post(post_data: PostCreate, user: dict = Depends(get_current_user)):
+    return posts_controller.create_post(post_data, user)
+
+@router.patch("/{postId}")
+async def update_post(postId: int, post_data: PostUpdate, user: dict = Depends(get_current_user)):
+    return posts_controller.update_post(postId, post_data, user)
+
+@router.delete("/{postId}")
+async def delete_post(postId: int, user: dict = Depends(get_current_user)):
+    return posts_controller.delete_post(postId, user)
